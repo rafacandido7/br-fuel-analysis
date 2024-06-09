ad72b999d024b4f88db3ed45946a9b52a651b249
rafacandido7 <rafacandido0708@gmail.com>
2024-06-09 15:40:28 -0300

    feat: upload files to minio

--- services/minio/minio.py
+++ services/minio/minio.py
@@ -5,32 +5,38 @@ from botocore.client import Config
 
 load_dotenv()
 
+MINIO_ENDPOINT = os.getenv('MINIO_ENDPOINT')
+MINIO_ACCESS_KEY_ID = os.getenv('MINIO_ACCESS_KEY_ID')
+MINIO_SECRET_ACCESS_KEY = os.getenv('MINIO_SECRET_ACCESS_KEY')
+MINIO_REGION = os.getenv('MINIO_REGION')
+MINIO_BUCKET_NAME = os.getenv('MINIO_BUCKET_NAME')
+
+
 s3 = boto3.client(
     's3',
-    endpoint_url=os.getenv('MINIO_ENDPOINT'),
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    endpoint_url=MINIO_ENDPOINT,
+    aws_access_key_id=MINIO_ACCESS_KEY_ID,
+    aws_secret_access_key=MINIO_SECRET_ACCESS_KEY,
     config=Config(signature_version='s3v4'),
-    region_name=os.getenv('AWS_REGION')
+    region_name=MINIO_REGION
 )
 
-bucket_name = os.getenv('AWS_BUCKET_NAME')
+def bucket_exists(bucket_name):
+    response = s3.list_buckets()
+    buckets = [bucket['Name'] for bucket in response['Buckets']]
+    return bucket_name in buckets
+
+def create_bucket_if_not_exists(bucket_name):
+    if not bucket_exists(bucket_name):
+        s3.create_bucket(Bucket=bucket_name)
+        print("Created bucket:", bucket_name)
+    else:
+        print("Bucket already exists:", bucket_name)
 
 def save_raw_data(file_paths):
+    create_bucket_if_not_exists(MINIO_BUCKET_NAME)
+    
     for file_path in file_paths:
-        object_name = os.path.basename(file_path)
-        print(object_name)
-        # send_file_to_minio(file_path, object_name)
-
-def send_file_to_minio(file_path, object_name):
-    s3.upload_file(file_path, object_name)
-
-# def file_exists(object_name):
-#     try:
-#         s3.head_object(Bucket=bucket_name, Key=object_name)
-#         return True
-#     except Error as e:
-#         if e.response['Error']['Code'] == '404':
-#             return False
-#         else:
-#             raise e
+        object_name = file_path.split('/').pop()
+        print("Uploading", file_path, "as", object_name)
+        s3.upload_file(file_path, MINIO_BUCKET_NAME, object_name)
