ae0eeecdc007b7673f2bd08177d0e98b79b777e7
Rafael Candido <rafacandido0708@gmail.com>
2024-06-15 16:56:34 -0300

    feat: dollar etl

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,7 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE dim_tempo (
-    tempo_id UUID PRIMARY KEY,
+    tempo_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     data_coleta DATE,
     dia INT,
     mes INT,
@@ -7,42 +9,35 @@ CREATE TABLE dim_tempo (
     dia_semana INT
 );
 
-CREATE TABLE dim_localizacao_geografica (
-    localizacao_geografica_id UUID PRIMARY KEY,
-    latitude DECIMAL,
-    longitude DECIMAL
-);
-
 CREATE TABLE dim_localizacao (
-    localizacao_id UUID PRIMARY KEY,
+    localizacao_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     regiao_sigla VARCHAR,
     estado_sigla VARCHAR,
     municipio VARCHAR,
     bairro VARCHAR,
-    cep VARCHAR,
-    localizacao_geografica_id UUID REFERENCES dim_localizacao_geografica(localizacao_geografica_id)
+    cep VARCHAR
 );
 
 CREATE TABLE dim_produto (
-    produto_id UUID PRIMARY KEY,
-    produto VARCHAR,
+    produto_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    produto VARCHAR
 );
 
 CREATE TABLE dim_revenda (
-    revenda_id UUID PRIMARY KEY,
+    revenda_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     cnpj_revenda VARCHAR,
     nome_revenda VARCHAR
 );
 
 CREATE TABLE dim_endereco (
-    endereco_id UUID PRIMARY KEY,
+    endereco_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     nome_rua VARCHAR,
     numero_rua VARCHAR,
     complemento VARCHAR
 );
 
 CREATE TABLE fato_venda (
-    venda_id UUID PRIMARY KEY,
+    venda_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     tempo_id UUID REFERENCES dim_tempo(tempo_id),
     localizacao_id UUID REFERENCES dim_localizacao(localizacao_id),
     produto_id UUID REFERENCES dim_produto(produto_id),
@@ -50,4 +45,14 @@ CREATE TABLE fato_venda (
     endereco_id UUID REFERENCES dim_endereco(endereco_id),
     valor_venda DECIMAL,
     valor_compra DECIMAL
+);
+
+CREATE TABLE dollar_info (
+    dollar_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    data DATE,
+    dia INT,
+    mes INT,
+    ano INT,
+    dia_semana INT,
+    valor DECIMAL
 );
