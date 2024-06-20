a67825df78b4866191a8a7397d499e92f058245b
Rafael Candido <rafacandido0708@gmail.com>
2024-06-20 02:43:06 -0300

    feat: geographic analysis by state

--- src/dashboard/pgs/geographic_analysis.py
+++ src/dashboard/pgs/geographic_analysis.py
@@ -15,7 +15,7 @@ DB_PROPERTIES = {
 def geographic_analysis_page():
     st.title("Análise Geográfica de Preços de Combustíveis")
 
-    st.header("Comparação de Preços entre Diferentes Regiões")
+    st.header("Comparação de Preços entre Diferentes Estados")
     selected_year = st.slider("Ano", 2004, 2023, 2023)
     fuel_types = ["GASOLINA", "GASOLINA ADITIVADA", "DIESEL S10", "DIESEL", "GNV", "ETANOL"]
     selected_fuel = st.selectbox("Tipo de Combustível", fuel_types)
@@ -30,25 +30,34 @@ def geographic_analysis_page():
     df_filtered = df_venda.join(df_tempo, "tempo_id").join(df_localizacao, "localizacao_id").join(df_produto, "produto_id")
     df_filtered = df_filtered.filter((df_tempo.ano == selected_year) & (df_produto.produto == selected_fuel))
 
-    df_grouped = df_filtered.groupBy("regiao_sigla").agg(f.mean("valor_venda").alias("preco_medio"))
+    df_grouped = df_filtered.groupBy("estado_sigla").agg(f.mean("valor_venda").alias("preco_medio"))
 
     df_pandas = df_grouped.toPandas()
 
-    fig = px.bar(df_pandas, x="regiao_sigla", y="preco_medio",
-                 title=f"Comparação de Preços de {selected_fuel} por Região em {selected_year}",
-                 labels={"regiao_sigla": "Região", "preco_medio": "Preço Médio de Venda"},
-                 color="regiao_sigla")
+    df_pandas = df_pandas.dropna()
+    df_pandas["preco_medio"] = df_pandas["preco_medio"].astype(float)
+
+    df_pandas = df_pandas.sort_values(by='estado_sigla')
+
+    fig = px.bar(df_pandas, x='estado_sigla', y='preco_medio',
+                 title=f'Comparação de Preços de {selected_fuel} por Estado em {selected_year}',
+                 labels={'estado_sigla': 'Estado', 'preco_medio': 'Preço Médio de Venda'},
+                 color='estado_sigla',
+                 height=600)
 
     fig.update_layout(
-        xaxis_title="Região",
-        yaxis_title="Preço Médio de Venda"
+        xaxis_title="Estado",
+        yaxis_title="Preço Médio de Venda",
+        yaxis=dict(tickformat=".2f"),
+        xaxis=dict(categoryorder='category ascending')
     )
 
     st.plotly_chart(fig)
 
     st.header("Observações")
     st.markdown(f"""
-    - **Fatores Influentes**: Fatores como custos de transporte, impostos regionais e demanda local podem influenciar essas variações de preços.
+    - **Variações por Estado**: Há variações significativas nos preços de {selected_fuel} entre os estados, influenciadas por fatores regionais e locais.
+    - **Fatores Influentes**: Custos de transporte, impostos estaduais e demanda local são fatores que podem contribuir para essas variações de preços.
     """)
 
 if __name__ == "__main__":
